8823fae7db685eb552d5b04534712e46ae237605
Roman Dolgushin <rd@roman-dolgushin.ru>
2012-11-16 10:34:37 +0600

    setup.py was updated

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
   license = 'MIT',
   version = '0.1',
   packages = ['syncle'],
+  install_requires = ['PyYAML'],
   entry_points = {
     'console_scripts': ['syncle = syncle.syncle:main']
   }
